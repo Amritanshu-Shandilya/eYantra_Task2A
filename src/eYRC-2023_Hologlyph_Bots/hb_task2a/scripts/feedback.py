94ba505ec7ff58274ca197daa9df2311529f3f47
Amritanshu Shandilya <sandyamritanshu9715@gmail.com>
2023-10-12 22:04:35 +0530

    did some basic work on the feedback.py file

--- src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
+++ src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
@@ -21,19 +21,37 @@ import rclpy
 from rclpy.node import Node
 
 # Import the required modules
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose2D #verification required
+
+import cv2 as cv
+from cv_bridge import CvBridge
+import numpy as np
+
 ##############################################################
 class ArUcoDetector(Node):
 
     def __init__(self):
         super().__init__('ar_uco_detector')
-
         # Subscribe the topic /camera/image_raw
+        self.r_image_subscriber = self.create_subscription(Image, '/camera/image_raw', self.image_callback, 1)
+        # Publish to topic /aruco_detection
+        self.create_publisher(Pose2D,'/detect_aruco',1)
+
+        # For maintaining control loop rate.
+        self.rate = self.create_rate(100)
 
 
     def image_callback(self, msg):
         #convert ROS image to opencv image
+        cv_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+
         #Detect Aruco marker
+        self.get_logger().info(str(cv.aruco.detect.markers(cv_image)))
+
         # Publish the bot coordinates to the topic  /detected_aruco
+        
 
 def main(args=None):
     rclpy.init(args=args)
