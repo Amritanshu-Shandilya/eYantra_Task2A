711a404e4820090da9949b5240bc54b759e0bf6e
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-10-18 14:40:06 +0000

    Added two functions to find center of aruco marker

--- src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
+++ src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
@@ -31,6 +31,29 @@ import cv2
 
 ##############################################################
 
+
+def marker_center_diag_intersection(corners):
+    # Calculate midpoints of the diagonals
+    diagonal1_x = (corners[0][0] + corners[2][0]) / 2
+    diagonal1_y = (corners[0][1] + corners[2][1]) / 2
+    diagonal2_x = (corners[1][0] + corners[3][0]) / 2
+    diagonal2_y = (corners[1][1] + corners[3][1]) / 2
+
+    # Calculate the center as the intersection of diagonals
+    center_x = (diagonal1_x + diagonal2_x) / 2
+    center_y = (diagonal1_y + diagonal2_y) / 2
+
+    return center_x, center_y
+
+def marker_center_coord_avg(corners):
+    # Calculate the average of X and Y coordinates
+    x_sum = sum(point[0] for point in corners)
+    y_sum = sum(point[1] for point in corners)
+    center_x = x_sum / len(corners)
+    center_y = y_sum / len(corners)
+    return center_x, center_y
+
+
 class ArUcoDetector(Node):
 
     def __init__(self):
@@ -56,13 +79,16 @@ class ArUcoDetector(Node):
 
         # Print detected aruco id with its corners
         for id, corner in zip(ids, corners):
-            print(id[0], [list(i) for i in corner[0]])
+            corner = corner[0]
+            diag_intersection = marker_center_diag_intersection(corner)
+            avg_intersection = marker_center_coord_avg(corner)
+            print(id[0], diag_intersection, avg_intersection)
         print()
 
         # Display the image with aruco marers using OpenCV
         image_with_markers = cv2.aruco.drawDetectedMarkers(cv_image, corners, ids)
-        cv2.imshow('Camera Image', image_with_markers)
-        cv2.waitKey(1)
+        # cv2.imshow('Camera Image', image_with_markers)
+        # cv2.waitKey(1)
 
         # Publish the bot coordinates to the topic  /detected_aruco
         # control_pose = Pose2D()
