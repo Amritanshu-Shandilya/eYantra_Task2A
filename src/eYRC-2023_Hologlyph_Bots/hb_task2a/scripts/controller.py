8ae2dbb3be6aba0bf5eb0001213621b378ee3344
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-11-05 13:53:54 +0000

    Fixed bot pos topic name

--- src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
+++ src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
@@ -66,7 +66,7 @@ class HBController(Node):
         super().__init__('hb_controller')
         
         # Initialze Subscriber
-        self.aruco_subscriber = self.create_subscription(Pose2D,'/detect_aruco',self.aruco_detect_callback, 10)
+        self.aruco_subscriber = self.create_subscription(Pose2D,'/detected_aruco',self.aruco_detect_callback, 10)
 
 	    #	Use the below given topics to generate motion for the robot.
         #   /hb_bot_1/left_wheel_force,
